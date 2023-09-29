6149379882382a7136f8dc20190aa5d13f81e03e
danilo012 <46241643+danilo012@users.noreply.github.com>
2023-09-30 04:54:44 +0800

    Update posts.js

--- src/backend/db/posts.js
+++ src/backend/db/posts.js
@@ -229,7 +229,7 @@ export const posts = [
     _id: "eaf91a20-5b28-a6b2-bae97d76681a",
     content:
       "We can’t get over. echozone is number 1 earning platform for us💙",
-    mediaURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHntdIPZQj4PU-U4pLhBHTJ4g566PbTDJ9_NuRFIQ&s=0",
+    mediaURL: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRHntdIPZQj4PU-U4pLhBHTJ4g566PbTDJ9_NuRFIQ&s=0.jpg",
     mediaAlt: "echozone image",
     likes: {
       likeCount: 3,
