facca27cdb1bff52ea4c958f62e152e967794e03
danilo012 <46241643+danilo012@users.noreply.github.com>
2023-09-30 20:08:23 +0800

    Update users.js

--- src/backend/db/users.js
+++ src/backend/db/users.js
@@ -19,7 +19,7 @@ export const users = [
       "https://res.cloudinary.com/dxnbnviuz/image/upload/w_400,f_auto,q_auto/v1686332490/socialMedia/programming-bg_znumg2.jpg",
     createdAt: "2023-09-29T01:06:00+05:30",
     updatedAt: formatDate(),
-    server : [
+    following: [
       {
         _id: "LCrc9f0Zl0",
         firstName: "jeryan",
