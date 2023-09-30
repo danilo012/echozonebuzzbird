2e650856e59916e524cb6948d3575610cb963b8e
danilo012 <46241643+danilo012@users.noreply.github.com>
2023-09-30 18:51:33 +0800

    Update users.js

--- src/backend/db/users.js
+++ src/backend/db/users.js
@@ -19,6 +19,11 @@ export const users = [
       "https://res.cloudinary.com/dxnbnviuz/image/upload/w_400,f_auto,q_auto/v1686332490/socialMedia/programming-bg_znumg2.jpg",
     createdAt: "2023-09-29T01:06:00+05:30",
     updatedAt: formatDate(),
+    freeserver: [
+      {
+        website: "https://echozoneph.online", 
+      }, 
+     ], 
     following: [
       {
         _id: "LCrc9f0Zl0",
