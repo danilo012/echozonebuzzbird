2dc4e22f3add082f84ac6d59aea8073d458f93bf
danilo012 <46241643+danilo012@users.noreply.github.com>
2023-10-23 06:44:02 +0800

    Create slot.jsx

---
+++ src/pages/authentication/formComponents/slot.jsx
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>reserve slot </title>
+  <link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+  <iframe src="https://share.paybiz.ph/items/LXl9Qt4sxrHpiFDSh4YiRJ" width="100%" height="500px" frameBorder="1" style="border: 0;"></iframe>
+
+  <!--
+  This script places a badge on your repl's full-browser view back to your repl's cover
+  page. Try various colors for the theme: dark, light, red, orange, yellow, lime, green,
+  teal, blue, blurple, magenta, pink!
+  -->
+
+</body>
+
+</html>
