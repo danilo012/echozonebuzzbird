4cfcdaf65cabe6682e6fea1416e1fa452645783e
danilo012 <46241643+danilo012@users.noreply.github.com>
2023-10-23 06:42:47 +0800

    Update users.js

--- src/backend/db/users.js
+++ src/backend/db/users.js
@@ -6,13 +6,362 @@ import { formatDate } from "../utils/authUtils";
 
 export const users = [
   {
+    _id: "member8",
+    firstName: "jimasher",
+    lastName: "sanjose",
+    password: "1234",
+    username: "jimasher",
+    bio: "FREEZING MONEY ₱0.00",
+    website: "https://echozoneph.online/coinage",
+    profileAvatar:
+      "https://z-m-scontent.fmnl26-2.fna.fbcdn.net/v/t39.30808-1/323622895_928917584760628_2088396219843437850_n.jpg?stp=c0.5000x0.5000f_dst-jpg_e15_p258x258_q60_tt1_u&efg=eyJ1cmxnZW4iOiJ1cmxnZW5fZnJvbV91cmwifQ&_nc_cid=5917&_nc_ad=z-m&_nc_rml=0&_nc_ht=z-m-scontent.fmnl26-2.fna&_nc_cat=107&_nc_ohc=NZlj7F4c6AAAX9AG9iW&ccb=1-7&_nc_sid=5fac6f&oh=00_AfAbgvNr31apV-NkGcECNVeWxhFkOM1RFxpUPlpSOM1ZHA&oe=651FBB66",
+    backgroundImage:
+      "https://res.cloudinary.com/dxnbnviuz/image/upload/w_400,f_auto,q_auto/v1686332490/socialMedia/programming-bg_znumg2.jpg",
+    createdAt: "2023-10-16T01:06:00+05:30",
+    updatedAt: formatDate(),
+   following: [],
+    followers: [
+      {
+        _id: "admin01",
+        firstName: "jeryan",
+        lastName: "royate",
+        username: "jeryan",
+        profileAvatar:
+          "https://z-m-scontent.fmnl34-1.fna.fbcdn.net/v/t39.30808-1/381688520_332897295782044_2540021010493742272_n.jpg?stp=c0.5000x0.5000",
+      },
+      {
+        _id: "member2",
+        firstName: "jayne",
+    lastName: "galleon",
+    password: "1234",
+    username: "jaynegalleon",
+        profileAvatar:
+      "https://z-m-scontent.fmnl26-1.fna.fbcdn.net/v/t39.30808-1/358546700_6443345049087556_3146661469288427411_n.jpg?stp=c0.5000x0.5000f_dst-jpg_e15_p258x258_q60_tt1_u&efg=eyJ1cmxnZW4iOiJ1cmxnZW5fZnJvbV91cmwifQ&_nc_cid=5917&_nc_ad=z-m&_nc_rml=0&_nc_ht=z-m-scontent.fmnl26-1.fna&_nc_cat=110&_nc_ohc=pH9uCUU7WFsAX9lhToE&ccb=1-7&_nc_sid=5fac6f&oh=00_AfB-xT01Gf9Igw64EygtJ77b9GsDAgN79rHVTorcw-uQmQ&oe=65201CC0",
+      },
+    ],
+          server: [
+            {
+        _id: "freeserver",
+        firstName: "FREE",
+        lastName: "SERVER",
+        username: "FREE SERVER",
+        website: "https://echozoneph.online/coinage", 
+        profileAvatar:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVryLZNUF2JVR3hOTZ4qe567y9qM2qd1vGOMxzkv09_hF-DgY4&s",
+         }, 
+          ], 
+          affiliate: [
+      {
+    _id: "affiliateaccount",
+    firstName: "AFFILIATE",
+    lastName: "ACCOUNT",
+    username: "AFFILIATE ACCOUNT",
+    profileAvatar:
+      "https://cs1.socpanel.com/cs1/panel_logos/mkiWTx2FoNbsZVZ7MsZ2OxVhMJqNAXekZGHEmJpg.jpg",
+      },
+    ], 
+    bookmarks: [],
+  },
+  {
+    _id: "member7",
+    firstName: "banjo",
+    lastName: "alday",
+    password: "1234",
+    username: "banjoalday",
+    bio: "FREEZING MONEY ₱0.00",
+    website: "https://echozoneph.online/coinage",
+    profileAvatar:
+      "https://z-m-scontent.fmnl26-2.fna.fbcdn.net/v/t39.30808-1/323622895_928917584760628_2088396219843437850_n.jpg?stp=c0.5000x0.5000f_dst-jpg_e15_p258x258_q60_tt1_u&efg=eyJ1cmxnZW4iOiJ1cmxnZW5fZnJvbV91cmwifQ&_nc_cid=5917&_nc_ad=z-m&_nc_rml=0&_nc_ht=z-m-scontent.fmnl26-2.fna&_nc_cat=107&_nc_ohc=NZlj7F4c6AAAX9AG9iW&ccb=1-7&_nc_sid=5fac6f&oh=00_AfAbgvNr31apV-NkGcECNVeWxhFkOM1RFxpUPlpSOM1ZHA&oe=651FBB66",
+    backgroundImage:
+      "https://res.cloudinary.com/dxnbnviuz/image/upload/w_400,f_auto,q_auto/v1686332490/socialMedia/programming-bg_znumg2.jpg",
+    createdAt: "2023-10-16T01:06:00+05:30",
+    updatedAt: formatDate(),
+   following: [],
+    followers: [
+      {
+        _id: "admin01",
+        firstName: "jeryan",
+        lastName: "royate",
+        username: "jeryan",
+        profileAvatar:
+          "https://z-m-scontent.fmnl34-1.fna.fbcdn.net/v/t39.30808-1/381688520_332897295782044_2540021010493742272_n.jpg?stp=c0.5000x0.5000",
+      },
+      {
+        _id: "member2",
+        firstName: "jayne",
+    lastName: "galleon",
+    password: "1234",
+    username: "jaynegalleon",
+        profileAvatar:
+      "https://z-m-scontent.fmnl26-1.fna.fbcdn.net/v/t39.30808-1/358546700_6443345049087556_3146661469288427411_n.jpg?stp=c0.5000x0.5000f_dst-jpg_e15_p258x258_q60_tt1_u&efg=eyJ1cmxnZW4iOiJ1cmxnZW5fZnJvbV91cmwifQ&_nc_cid=5917&_nc_ad=z-m&_nc_rml=0&_nc_ht=z-m-scontent.fmnl26-1.fna&_nc_cat=110&_nc_ohc=pH9uCUU7WFsAX9lhToE&ccb=1-7&_nc_sid=5fac6f&oh=00_AfB-xT01Gf9Igw64EygtJ77b9GsDAgN79rHVTorcw-uQmQ&oe=65201CC0",
+      },
+    ],
+          server: [
+            {
+        _id: "freeserver",
+        firstName: "FREE",
+        lastName: "SERVER",
+        username: "FREE SERVER",
+        website: "https://echozoneph.online/coinage", 
+        profileAvatar:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVryLZNUF2JVR3hOTZ4qe567y9qM2qd1vGOMxzkv09_hF-DgY4&s",
+         }, 
+          ], 
+          affiliate: [
+      {
+    _id: "affiliateaccount",
+    firstName: "AFFILIATE",
+    lastName: "ACCOUNT",
+    username: "AFFILIATE ACCOUNT",
+    profileAvatar:
+      "https://cs1.socpanel.com/cs1/panel_logos/mkiWTx2FoNbsZVZ7MsZ2OxVhMJqNAXekZGHEmJpg.jpg",
+      },
+    ], 
+    bookmarks: [],
+  },
+  {
+    _id: "member6",
+    firstName: "cyrell",
+    lastName: "lapitan",
+    password: "1234",
+    username: "Cyrellreyes",
+    bio: "FREEZING MONEY ₱0.00",
+    website: "https://echozoneph.online/coinage",
+    profileAvatar:
+      "https://z-m-scontent.fmnl26-2.fna.fbcdn.net/v/t39.30808-1/323622895_928917584760628_2088396219843437850_n.jpg?stp=c0.5000x0.5000f_dst-jpg_e15_p258x258_q60_tt1_u&efg=eyJ1cmxnZW4iOiJ1cmxnZW5fZnJvbV91cmwifQ&_nc_cid=5917&_nc_ad=z-m&_nc_rml=0&_nc_ht=z-m-scontent.fmnl26-2.fna&_nc_cat=107&_nc_ohc=NZlj7F4c6AAAX9AG9iW&ccb=1-7&_nc_sid=5fac6f&oh=00_AfAbgvNr31apV-NkGcECNVeWxhFkOM1RFxpUPlpSOM1ZHA&oe=651FBB66",
+    backgroundImage:
+      "https://res.cloudinary.com/dxnbnviuz/image/upload/w_400,f_auto,q_auto/v1686332490/socialMedia/programming-bg_znumg2.jpg",
+    createdAt: "2023-10-16T01:06:00+05:30",
+    updatedAt: formatDate(),
+   following: [],
+    followers: [
+      {
+        _id: "admin01",
+        firstName: "jeryan",
+        lastName: "royate",
+        username: "jeryan",
+        profileAvatar:
+          "https://z-m-scontent.fmnl34-1.fna.fbcdn.net/v/t39.30808-1/381688520_332897295782044_2540021010493742272_n.jpg?stp=c0.5000x0.5000",
+      },
+      {
+        _id: "member2",
+        firstName: "jayne",
+    lastName: "galleon",
+    password: "1234",
+    username: "jaynegalleon",
+        profileAvatar:
+      "https://z-m-scontent.fmnl26-1.fna.fbcdn.net/v/t39.30808-1/358546700_6443345049087556_3146661469288427411_n.jpg?stp=c0.5000x0.5000f_dst-jpg_e15_p258x258_q60_tt1_u&efg=eyJ1cmxnZW4iOiJ1cmxnZW5fZnJvbV91cmwifQ&_nc_cid=5917&_nc_ad=z-m&_nc_rml=0&_nc_ht=z-m-scontent.fmnl26-1.fna&_nc_cat=110&_nc_ohc=pH9uCUU7WFsAX9lhToE&ccb=1-7&_nc_sid=5fac6f&oh=00_AfB-xT01Gf9Igw64EygtJ77b9GsDAgN79rHVTorcw-uQmQ&oe=65201CC0",
+      },
+    ],
+          server: [
+            {
+        _id: "freeserver",
+        firstName: "FREE",
+        lastName: "SERVER",
+        username: "FREE SERVER",
+        website: "https://echozoneph.online/coinage", 
+        profileAvatar:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVryLZNUF2JVR3hOTZ4qe567y9qM2qd1vGOMxzkv09_hF-DgY4&s",
+         }, 
+          ], 
+          affiliate: [
+      {
+    _id: "affiliateaccount",
+    firstName: "AFFILIATE",
+    lastName: "ACCOUNT",
+    username: "AFFILIATE ACCOUNT",
+    profileAvatar:
+      "https://cs1.socpanel.com/cs1/panel_logos/mkiWTx2FoNbsZVZ7MsZ2OxVhMJqNAXekZGHEmJpg.jpg",
+      },
+    ], 
+    bookmarks: [],
+  },
+  {
+    _id: "member1",
+    firstName: "vincent",
+    lastName: "bernabe",
+    password: "1234",
+    username: "Liza2520",
+    bio: "FREEZING MONEY ₱0.00",
+    website: "https://echozoneph.online/coinage",
+    profileAvatar:
+      "#",
+    backgroundImage:
+      "https://res.cloudinary.com/dxnbnviuz/image/upload/w_400,f_auto,q_auto/v1686332490/socialMedia/programming-bg_znumg2.jpg",
+    createdAt: "2023-09-29T01:06:00+05:30",
+    updatedAt: formatDate(),
+   following: [],
+    followers: [
+      {
+        _id: "admin01",
+        firstName: "jeryan",
+        lastName: "royate",
+        username: "jeryan",
+        profileAvatar:
+          "https://z-m-scontent.fmnl34-1.fna.fbcdn.net/v/t39.30808-1/381688520_332897295782044_2540021010493742272_n.jpg?stp=c0.5000x0.5000",
+      },
+    ],
+    server: [
+      {
+        _id: "freeserver",
+        firstName: "FREE",
+        lastName: "SERVER",
+        username: "FREE SERVER",
+        website: "https://lovely-stroopwafel-4c610d.netlify.app/game/?loadStatus=success", 
+        profileAvatar:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVryLZNUF2JVR3hOTZ4qe567y9qM2qd1vGOMxzkv09_hF-DgY4&s",
+     }, 
+      
+    ],
+    affiliate: [
+      {
+    _id: "affiliateaccount",
+    firstName: "AFFILIATE",
+    lastName: "ACCOUNT",
+    username: "AFFILIATE ACCOUNT",
+    profileAvatar:
+      "https://cs1.socpanel.com/cs1/panel_logos/mkiWTx2FoNbsZVZ7MsZ2OxVhMJqNAXekZGHEmJpg.jpg",
+      },
+    ], 
+    bookmarks: [],
+  },
+  {
+    _id: "member2",
+    firstName: "jayne",
+    lastName: "galleon",
+    password: "1234",
+    username: "jaynegalleon",
+    bio: "FREEZING MONEY ₱0.00",
+    website: "https://echozoneph.online/coinage",
+    profileAvatar:
+      "https://z-m-scontent.fmnl26-1.fna.fbcdn.net/v/t39.30808-1/358546700_6443345049087556_3146661469288427411_n.jpg?stp=c0.5000x0.5000f_dst-jpg_e15_p258x258_q60_tt1_u&efg=eyJ1cmxnZW4iOiJ1cmxnZW5fZnJvbV91cmwifQ&_nc_cid=5917&_nc_ad=z-m&_nc_rml=0&_nc_ht=z-m-scontent.fmnl26-1.fna&_nc_cat=110&_nc_ohc=pH9uCUU7WFsAX9lhToE&ccb=1-7&_nc_sid=5fac6f&oh=00_AfB-xT01Gf9Igw64EygtJ77b9GsDAgN79rHVTorcw-uQmQ&oe=65201CC0",
+    backgroundImage:
+      "https://res.cloudinary.com/dxnbnviuz/image/upload/w_400,f_auto,q_auto/v1686332490/socialMedia/programming-bg_znumg2.jpg",
+    createdAt: "2023-10-02T01:06:00+05:30",
+    updatedAt: formatDate(),
+   following: [],
+    followers: [
+      {
+        _id: "admin01",
+        firstName: "jeryan",
+        lastName: "royate",
+        username: "jeryan", 
+        profileAvatar:
+          "https://z-m-scontent.fmnl34-1.fna.fbcdn.net/v/t39.30808-1/381688520_332897295782044_2540021010493742272_n.jpg?stp=c0.5000x0.5000",
+      },
+      {
+        _id: "1T6Be1QpLm",
+        firstName: "Jacob",
+        lastName: "Mitchell",
+        username: "jacobmitch",
+        profileAvatar:
+          "https://res.cloudinary.com/dxnbnviuz/image/upload/w_400,f_auto,q_auto/v1686331002/socialMedia/Jacob-Mitchell_elh9gg.jpg",
+      },
+      {
+        _id: "M1NR81Bert",
+    firstName: "Ava",
+    lastName: "Turner",
+    username: "avaturn",
+    profileAvatar:
+      "https://res.cloudinary.com/dxnbnviuz/image/upload/w_400,f_auto,q_auto/v1686331028/socialMedia/Ava-Turner_vqavsz.jpg",
+      }, 
+      {
+    _id: "member3",
+    firstName: "JOVELIZA",
+    lastName: "BARLAAN",
+    username: "Juvellz",
+    profileAvatar:
+      "https://z-m-scontent.fmnl26-2.fna.fbcdn.net/v/t39.30808-1/323622895_928917584760628_2088396219843437850_n.jpg?stp=c0.5000x0.5000f_dst-jpg_e15_p258x258_q60_tt1_u&efg=eyJ1cmxnZW4iOiJ1cmxnZW5fZnJvbV91cmwifQ&_nc_cid=5917&_nc_ad=z-m&_nc_rml=0&_nc_ht=z-m-scontent.fmnl26-2.fna&_nc_cat=107&_nc_ohc=NZlj7F4c6AAAX9AG9iW&ccb=1-7&_nc_sid=5fac6f&oh=00_AfAbgvNr31apV-NkGcECNVeWxhFkOM1RFxpUPlpSOM1ZHA&oe=651FBB66",
+      }, 
+    ],
+    server: [
+      {
+        _id: "freeserver",
+        firstName: "FREE",
+        lastName: "SERVER",
+        username: "FREE SERVER",
+        website: "https://echozoneph.online/coinage", 
+        profileAvatar:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVryLZNUF2JVR3hOTZ4qe567y9qM2qd1vGOMxzkv09_hF-DgY4&s",
+     }, 
+    ],
+        affiliate: [
+      {
+    _id: "affiliateaccount",
+    firstName: "AFFILIATE",
+    lastName: "ACCOUNT",
+    username: "AFFILIATE ACCOUNT",
+    profileAvatar:
+      "https://cs1.socpanel.com/cs1/panel_logos/mkiWTx2FoNbsZVZ7MsZ2OxVhMJqNAXekZGHEmJpg.jpg",
+      },
+    ], 
+    bookmarks: [],
+  },
+        {
+    _id: "member3",
+    firstName: "JOVELIZA",
+    lastName: "BARLAAN",
+    password: "1234",
+    username: "Juvellz",
+    bio: "FREEZING MONEY ₱0.00",
+    website: "https://echozoneph.online/coinage",
+    profileAvatar:
+      "https://z-m-scontent.fmnl26-2.fna.fbcdn.net/v/t39.30808-1/323622895_928917584760628_2088396219843437850_n.jpg?stp=c0.5000x0.5000f_dst-jpg_e15_p258x258_q60_tt1_u&efg=eyJ1cmxnZW4iOiJ1cmxnZW5fZnJvbV91cmwifQ&_nc_cid=5917&_nc_ad=z-m&_nc_rml=0&_nc_ht=z-m-scontent.fmnl26-2.fna&_nc_cat=107&_nc_ohc=NZlj7F4c6AAAX9AG9iW&ccb=1-7&_nc_sid=5fac6f&oh=00_AfAbgvNr31apV-NkGcECNVeWxhFkOM1RFxpUPlpSOM1ZHA&oe=651FBB66",
+    backgroundImage:
+      "https://res.cloudinary.com/dxnbnviuz/image/upload/w_400,f_auto,q_auto/v1686332490/socialMedia/programming-bg_znumg2.jpg",
+    createdAt: "2023-10-02T01:06:00+05:30",
+    updatedAt: formatDate(),
+   following: [],
+    followers: [
+      {
+        _id: "admin01",
+        firstName: "jeryan",
+        lastName: "royate",
+        username: "jeryan",
+        profileAvatar:
+          "https://z-m-scontent.fmnl34-1.fna.fbcdn.net/v/t39.30808-1/381688520_332897295782044_2540021010493742272_n.jpg?stp=c0.5000x0.5000",
+      },
+      {
+        _id: "member2",
+        firstName: "jayne",
+    lastName: "galleon",
+    password: "1234",
+    username: "jaynegalleon",
+        profileAvatar:
+      "https://z-m-scontent.fmnl26-1.fna.fbcdn.net/v/t39.30808-1/358546700_6443345049087556_3146661469288427411_n.jpg?stp=c0.5000x0.5000f_dst-jpg_e15_p258x258_q60_tt1_u&efg=eyJ1cmxnZW4iOiJ1cmxnZW5fZnJvbV91cmwifQ&_nc_cid=5917&_nc_ad=z-m&_nc_rml=0&_nc_ht=z-m-scontent.fmnl26-1.fna&_nc_cat=110&_nc_ohc=pH9uCUU7WFsAX9lhToE&ccb=1-7&_nc_sid=5fac6f&oh=00_AfB-xT01Gf9Igw64EygtJ77b9GsDAgN79rHVTorcw-uQmQ&oe=65201CC0",
+      },
+    ],
+          server: [
+            {
+        _id: "freeserver",
+        firstName: "FREE",
+        lastName: "SERVER",
+        username: "FREE SERVER",
+        website: "https://echozoneph.online/coinage", 
+        profileAvatar:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVryLZNUF2JVR3hOTZ4qe567y9qM2qd1vGOMxzkv09_hF-DgY4&s",
+         }, 
+          ], 
+          affiliate: [
+      {
+    _id: "affiliateaccount",
+    firstName: "AFFILIATE",
+    lastName: "ACCOUNT",
+    username: "AFFILIATE ACCOUNT",
+    profileAvatar:
+      "https://cs1.socpanel.com/cs1/panel_logos/mkiWTx2FoNbsZVZ7MsZ2OxVhMJqNAXekZGHEmJpg.jpg",
+      },
+    ], 
+    bookmarks: [],
+  },
+  {
     _id: "admin01",
     firstName: "jeryan",
     lastName: "royate",
-    password: "1234",
+    password: "101017",
     username: "jeryan",
-    bio: "SERVER ACCESS (INACTIVE)",
-    website: "https://echozoneph.online",
+    bio: "FREEZING MONEY ₱0.00",
+    website: "https://echozoneph.online/coinage",
     profileAvatar:
       "https://z-m-scontent.fmnl34-1.fna.fbcdn.net/v/t39.30808-1/381688520_332897295782044_2540021010493742272_n.jpg?stp=c0.5000x0.5000f_dst-jpg_e15_p96x96_q60_tt1_u&efg=eyJ1cmxnZW4iOiJ1cmxnZW5fZnJvbV91cmwifQ&_nc_cid=5917&_nc_ad=z-m&_nc_rml=0&_nc_ht=z-m-scontent.fmnl34-1.fna&_nc_cat=100&_nc_ohc=cyY7_1UqTekAX8WHqXP&ccb=1-7&_nc_sid=5fac6f&oh=00_AfBoVNiCLh8ib1APu2RXrJ1fIoiCO-x364sOrRU1kzh2KA&oe=651C8E5E",
     backgroundImage:
@@ -21,13 +370,13 @@ export const users = [
     updatedAt: formatDate(),
    following: [
       {
-        _id: "LCrc9f0Zl0",
+        _id: "admin01",
         firstName: "jeryan",
         lastName: "royate",
-        username: "freeserver",
+        username: "jeryan",
         website: "http://echozoneph.online", 
         profileAvatar:
-          "https://res.cloudinary.com/dxnbnviuz/image/upload/w_400,f_auto,q_auto/v1686331041/socialMedia/Aditya-Joshi_ajn3sy.jpg",
+          "https://z-m-scontent.fmnl34-1.fna.fbcdn.net/v/t39.30808-1/381688520_332897295782044_2540021010493742272_n.jpg?stp=c0.5000x0.5000",
       },
       {
         _id: "1T6Be1QpLm",
@@ -72,8 +421,38 @@ export const users = [
           "https://res.cloudinary.com/dxnbnviuz/image/upload/w_400,f_auto,q_auto/v1686331058/socialMedia/Olivia-Parker_nbmkdw.jpg",
       },
     ],
+    affiliate: [
+      {
+    _id: "affiliateaccount",
+    firstName: "AFFILIATE",
+    lastName: "ACCOUNT",
+    username: "AFFILIATE ACCOUNT",
+    profileAvatar:
+      "https://cs1.socpanel.com/cs1/panel_logos/mkiWTx2FoNbsZVZ7MsZ2OxVhMJqNAXekZGHEmJpg.jpg",
+      },
+    ], 
     bookmarks: [
     ],
+    server: [
+      {
+        _id: "freeserver",
+        firstName: "FREE",
+        lastName: "SERVER",
+        username: "FREE SERVER",
+        website: "https://freeserver-2-ph.netlify.app/farm1index.html", 
+        profileAvatar:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVryLZNUF2JVR3hOTZ4qe567y9qM2qd1vGOMxzkv09_hF-DgY4&s",
+     }, 
+      {
+        _id: "normalserver",
+        firstName: "NORMAL",
+        lastName: "SERVER",
+        username: "NORMAL SERVER",
+        website: "http://echocloud.mypressonline.com/login.php", 
+        profileAvatar:
+          "https://miro.medium.com/v2/resize:fit:1400/0*SugiK9RuRcXPIZiG.png",
+     }, 
+     ], 
   },
   {
     _id: "79Gksh9otl",
@@ -325,5 +704,50 @@ export const users = [
     following: [],
     followers: [],
     bookmarks: [],
+  },
+  {
+    _id: "normalserver",
+    firstName: "NORMAL",
+    lastName: "SERVER",
+    password: "server",
+    username: "NORMAL SERVER",
+    bio: "SERVER ACCESS (click here👇)",
+    website: "",
+    profileAvatar:
+      "https://miro.medium.com/v2/resize:fit:1400/0*SugiK9RuRcXPIZiG.png",
+    backgroundImage:
+      "https://miro.medium.com/v2/resize:fit:1400/0*SugiK9RuRcXPIZiG.png",
+    createdAt: "2023-10-01T01:06:00+05:30",
+    updatedAt: formatDate(),
+  },
+    {
+    _id: "freeserver",
+    firstName: "FREE",
+    lastName: "SERVER",
+    password: "server",
+    username: "FREE SERVER",
+    bio: "SERVER ACCESS (click here👇)",
+    website: "https://echozoneph.online/coinagfreeserver.html",
+    profileAvatar:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVryLZNUF2JVR3hOTZ4qe567y9qM2qd1vGOMxzkv09_hF-DgY4&s",
+    backgroundImage:
+      "https://miro.medium.com/v2/resize:fit:1400/0*SugiK9RuRcXPIZiG.png",
+    createdAt: "2023-10-01T01:06:00+05:30",
+    updatedAt: formatDate(),
+  },
+  {
+    _id: "affiliateaccount",
+    firstName: "AFFILIATE",
+    lastName: "ACCOUNT",
+    password: "server",
+    username: "AFFILIATE ACCOUNT",
+    bio: "GO TO AFFILIATE PANEL (click here👇)",
+    website: "https://echozone.socpanel.com/dashboard",
+    profileAvatar:
+      "https://cs1.socpanel.com/cs1/panel_logos/mkiWTx2FoNbsZVZ7MsZ2OxVhMJqNAXekZGHEmJpg.jpg",
+    backgroundImage:
+      "https://cs1.socpanel.com/cs1/panel_logos/mkiWTx2FoNbsZVZ7MsZ2OxVhMJqNAXekZGHEmJpg.jpg",
+    createdAt: "2023-10-01T01:06:00+05:30",
+    updatedAt: formatDate(),
   },
 ];
